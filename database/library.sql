9ef22f7ab638d8a940d01e4e208a9be11aa3e08d
Sven Frei <frei-sven@bluewin.ch>
2022-12-22 17:09:56 +0800

    fix sql

--- database/library.sql
+++ database/library.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `library`
 --
-
-Create Database library;
+CREATE DATABASE IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `library`;
 
 -- --------------------------------------------------------
 
