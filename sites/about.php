486acc908733756d1405da1a0fb6e539000f6431
neallylove <107135328+NurHidayah-85@users.noreply.github.com>
2023-01-06 22:51:12 +0800

    meet the team progress

--- sites/about.php
+++ sites/about.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head><link rel="stylesheet" type="text/css" href="css/mystyle.css"> <!--link to mystyle.css --></head>
+    <head><link rel="stylesheet" type="text/css" href="css/mystyle.css"> <!--link to mystyle.css -->
+<style>
+    table { box-shadow: 1px 1px 1px #999; }
+    th, td {
+    font-size: 15px;
+  }
+</style>
+</head>
     <body>
-  <p><h1> About Us</h1><br>
+  <p><h1> About KKSL Library</h1><br>
   &nbsp&nbsp&nbsp&nbsp&nbsp&nbspKolej Komuniti Selandar (KKSL) began operating in 2003 and was temporarily located at
 Sekolah Menengah Kebangsaan Nyalas, Malacca. KKSL began moving to its own permanent
 campus in July 2004 at Jalan Batang Melaka Selandar, Malacca. KKSL has an area of 7 acres with
@@ -21,6 +28,73 @@ student or employee card at the counter. The borrowing duration for one book is 
 members. </p><br><br>
 
 <p><h1> Meet the Team</h1><br>
+<table>
+<tr align="center">
+        <td>picture here</td>
+        
+</tr>
+<tr align="center" bgcolor="#F4D03F"  >
+        <td>Nur Hidayah Binti Ishak</td>
+       
+</tr>
+<tr align="center">
+        <td>2020496204</td>
+ 
+</tr>
+<tr align="center">
+        <td>2020496204@student.uitm.edu.my</td>
+    
+</tr>
+</table>
+<table>
+<tr align="center">
+        <td>picture here</td>
+        
+</tr>
+<tr align="center" bgcolor="#F4D03F"  >
+        <td>Nur Hidayah Binti Ishak</td>
+       
+</tr>
+<tr align="center">
+        <td>2020496204</td>
+ 
+</tr>
+<tr align="center">
+        <td>2020496204@student.uitm.edu.my</td>
+    
+</tr>
+</table>
+<table>
+<tr align="center">
+        <td>picture here</td>
+        <td>picture here</td>
+        <td>picture here</td>
+        <td>picture here</td>
+</tr>
+<tr align="center" bgcolor="#F4D03F"  >
+        <td>Nur Hidayah Binti Ishak</td>
+        <td>Frei Sven</td>
+        <td>Muhammad Nur Arif Bin Mohd Azman</td>
+        <td>Na’bil Bin Saperi</td>
+</tr>
+<tr align="center">
+        <td>2020496204</td>
+        <td>2022828866</td>
+        <td>2021619936</td>
+        <td>2022971889</td>
+</tr>
+<tr align="center">
+        <td>2020496204@student.uitm.edu.my</td>
+        <td>2022828866@student.uitm.edu.my </td>
+        <td>2021619936@student.uitm.edu.my </td>
+        <td>2021619936@student.uitm.edu.my </td>
+</tr>
+</table>
+
+
+
+
+
 
     </body>
 </html>
