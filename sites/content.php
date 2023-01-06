4c55567c89d365b1e0d679d9c64b34b7876cfe9d
neallylove <107135328+NurHidayah-85@users.noreply.github.com>
2023-01-07 01:43:17 +0800

    update content

--- sites/content.php
+++ sites/content.php
@@ -1,8 +1,65 @@
 <!DOCTYPE html>
 <html>
-    <head><link rel="stylesheet" type="text/css" href="css/mystyle.css"> <!--link to mystyle.css --></head>
+    <head><link rel="stylesheet" type="text/css" href="css/mystyle.css"> <!--link to mystyle.css -->
+
+<style>
+/* img {
+  float: left;
+  padding: 10px;
+}*/
+
+img {
+        max-width: 100%;
+       
+      }
+      .image {
+        flex-basis: 80%
+      }
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #F9B219;
+  text-align: center;
+  cursor: pointer;
+  width: 80%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+.container {
+        display: flex;
+        align-items: center;
+        justify-content: center
+      }
+
+      .text {
+        
+        padding-left: 20px;
+        
+
+      }
+
+
+</style>
+</head>
     <body>
-   <p align = "left"> <img src="images/home_picture.jpg" alt="bookracks"  style="width:70%;" class="center">
-</p>    
+
+<!-- <p><img src="images/home_picture.jpg" alt="bookracks"  style="width:70%;" class="center"></p>
+<p style= "padding: 10px;" align="center" class="container">Welcome to KKSL Library <br>
+Your one stop center </p><p align="center"><br><a href="facilities.php" target="content"><button class="button">Explore more</button></a></p> -->
+
+
+<div class="container">
+<div class="image"><img src="images/home_picture.jpg" alt="bookracks"></td>
+        </div>
+        <div class="text">
+        <p align="center" >Welcome to KKSL Library <br>
+Your one stop center <br><a href="facilities.php" target="content"><button class="button">Explore more</button></a></p>
+  </div>
+</div>
     </body>
 </html>
