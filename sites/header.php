dfd105b488ce9147119debf1d03ddbf4b6745a6e
neallylove <107135328+NurHidayah-85@users.noreply.github.com>
2023-01-06 15:49:04 +0800

    update header

--- sites/header.php
+++ sites/header.php
@@ -3,14 +3,15 @@
     <head><link rel="stylesheet" type="text/css" href="css/mystyle.css"> <!--link to mystyle.css -->
     <style>
         td:hover {background-color: white}
+        
         </style>
             </head>
     <body>
    <img src="images/kksllogo.png" alt="logo" width="200" height="90">
-    <table style="width:100%; color:white;" border=0 bgcolor=red >
+    <table style="width:100%; font-color:white;" border=0 bgcolor=red >
        
         <tr>
-        <td align="center"><b>Home</b></td>
+        <td align="center"><a target="_top" href="index.php"><b>Home</b></a></td>
         <td align="center"><b>Books</b></td>
         <td align="center"><b>Facilities</b></td>
         <td align="center"><b>Login</b></td>
