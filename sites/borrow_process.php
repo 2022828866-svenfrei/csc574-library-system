cbbb7610c5dcce50387e5d3af5919a2efd142138
neallylove <107135328+NurHidayah-85@users.noreply.github.com>
2023-01-25 15:20:58 +0800

    borrow

--- sites/borrow_process.php
+++ sites/borrow_process.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Borrow Status</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyle.css"> <!--link to mystyle.css -->
+    <link rel="stylesheet" type="text/css" href="css/userstyle.css"> <!--link to userstyle.css -->
+
+    <style>
+        .borrowingTable {
+            border: 1px solid;
+        }
+    </style>
+</head>
+
+<body>
+
 <?php
 include 'repositories/conn.php';
 require("repositories/library-repository.php");
@@ -14,7 +31,12 @@ $sql = "INSERT INTO `borrow`(`BookFK`, `UserFK`, `FromDate`, `ToDate`) VALUES ($
 if (!mysqli_query($conn, $sql)) {
     die('Error: ' . mysqli_error($conn));
 }
-echo "1 record successfully added..<br>";
-echo "<a href=listForum.php>Display all</a>";
+echo "<p align='center'><h1>Awesome!</h1></p>";
+echo "<p align= 'center'><img src='images/successicon.png' alt='success2' width='250' height='250'><br></p>";
+echo "<p align='center'>You successfully created booking<br></p>";
+echo "<p align='center'>To see your booking <a href=personal.php>--Click Here--</a></p>";
 
 ?>
+</body>
+
+</html>
