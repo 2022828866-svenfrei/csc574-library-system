480da9cc9fd59b5389d26f31ceab0dc3f18ced17
neallylove <107135328+NurHidayah-85@users.noreply.github.com>
2023-01-25 20:44:44 +0800

    update picture

--- sites/literature.php
+++ sites/literature.php
@@ -1,22 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Personal Page</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyle.css"> <!--link to mystyle.css -->
+
+</head>
+
+<body>
 <?php
 include 'repositories/conn.php';
-/*echo "<table border='1'><tr><th>Name</th><th>Category</th>";
-echo "<th>Publish Date</th><th>Publish Place</th>";
-echo "<th>Author</th><th>Description</th>";
-echo "<th>ISBN</th><th>Picture</th>";
-echo "<th>View</th><th>Booking</th></tr>";*/
+/
 echo "<table>";
 $result = mysqli_query($conn, "SELECT * FROM `book` WHERE `Category` = 'Literature';");
 while($row = mysqli_fetch_row($result)){
-    echo "<tr><td>PICTURE HERE $row[7]</td>";
-    echo "<td>&nbsp Book Name: $row[1]</td>";
-    echo "<td>&nbsp Category: $row[2]</td>";
-    echo "<td>&nbspAuthor: $row[4]</td>";
-    echo "<td>&nbsp<a href=viewdetailbook.php?ID=$row[0]>View More<a></td>";
-    echo "<td style='color:red'><a href=borrow.php?ID=$row[0]>Borrow Now</td>";
-    echo "</tr><br>";
+    echo '<tr><td><img style="height: 100px; width: 100px;" src="images/uploads/'.$row[7].'"></td>';
+    echo "<td>&nbsp ||Book Name: $row[1]</td>";
+    echo "<td>&nbsp ||&nbspCategory: $row[2]</td>";
+    echo "<td>&nbsp ||&nbspAuthor: $row[4]</td>";
+    echo "<td>&nbsp<a href=viewdetailbook.php?ID=$row[0]><button type='button'>View More</button><a></td>";
+    echo "<td style='color:red'><a href=borrow.php?ID=$row[0]><button type='button'>Borrow Now</button></td>";
+    echo "</tr>";
     }
 echo "</table>";
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
+</body>
+
+</html>
