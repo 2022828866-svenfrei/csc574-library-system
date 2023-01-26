16bea484919b492435d77da37d366adf2a2a77df
nurarifazman <arifazman011@gmail.com>
2023-01-26 11:36:27 +0800

    Update library.sql

--- database/library.sql
+++ database/library.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 25, 2023 at 11:06 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Jan 26, 2023 at 04:34 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `library`
 --
-CREATE DATABASE IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `library`;
 
 -- --------------------------------------------------------
 
@@ -29,7 +27,6 @@ USE `library`;
 -- Table structure for table `book`
 --
 
-DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book` (
   `ID` int(11) NOT NULL,
   `Name` varchar(100) NOT NULL,
@@ -41,7 +38,7 @@ CREATE TABLE `book` (
   `Image` text NOT NULL,
   `PublishPlace` varchar(100) NOT NULL,
   `Price` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `book`
@@ -49,7 +46,10 @@ CREATE TABLE `book` (
 
 INSERT INTO `book` (`ID`, `Name`, `Category`, `PublishDate`, `Author`, `Description`, `ISBNNumber`, `Image`, `PublishPlace`, `Price`) VALUES
 (1, 'Book 1', 'Drama', '2022-08-03', 'Sven', 'Desc.', '913939', 'Book 1.jpg', 'Switzerland', '150'),
-(4, 'Power Parenting: Motivasi Anak Pintar', 'General', '2023-01-23', 'Tengku Asmadi', 'Setiap ibu bapa', '9789830976099', 'Power Parenting: Motivasi Anak Pintar.jpg', 'Selangor', '35');
+(4, 'Power Parenting: Motivasi Anak Pintar', 'General', '2023-01-23', 'Tengku Asmadi', 'Setiap ibu bapa', '9789830976099', 'Power Parenting: Motivasi Anak Pintar.jpg', 'Selangor', '35'),
+(5, 'Amalan Ibu Mengandung', 'General', '2023-01-03', 'Dato’ Siti Nor Bahyah Mahamood', 'Kehamilan adalah satu keistimewa hanya milik kaum wanita. Hamil bukan satu bebanan tetapi satu harapan, kesabaran dan kebahagiaan bagi seorang ibu. Allah menjanjikan pahala yang berlipat ganda bagi seorang ibu yang hamil. Sahabat Rasulullah saw dalam keadaan sangat miskin dan papa sekalipun, tetap berkeinginan mempunyai anak yang ramai. Buku ini mengandungi amalan dan petua yang bersandarkan daripada al-Quran, hadis dan pendapat ulama termasuk juga daripada pemerhatian orang soleh dan panduan daripada ilmu perubatan, rawatan perbidanan dan pemakanan moden. Semua panduan disertakan gambar warna penuh fotografi, lukisan, carta, jadual dan info grafik untuk menerangan kaedah yang paling tepat supaya mudah difahami dan diamalkan.', '978-967-481-791-6', 'Amalan Ibu Mengandung.jpg', 'Selangor', '25'),
+(6, 'test', 'test2', '2023-01-26', 'test5', 'test3', 'test6', 'test.jpg', 'test4', '25'),
+(7, 'Mrs Jay', 'General', '2023-01-11', 'Dato’ Siti Nor Bahyah Mahamood', ',,,\"\"\"\"', '9789671754382', 'Mrs Jay.jpg', 'Indonesia', '28');
 
 -- --------------------------------------------------------
 
@@ -57,25 +57,50 @@ INSERT INTO `book` (`ID`, `Name`, `Category`, `PublishDate`, `Author`, `Descript
 -- Table structure for table `borrow`
 --
 
-DROP TABLE IF EXISTS `borrow`;
 CREATE TABLE `borrow` (
   `ID` int(11) NOT NULL,
   `BookFK` int(11) NOT NULL,
   `UserFK` int(11) NOT NULL,
   `FromDate` date NOT NULL,
   `ToDate` date NOT NULL,
-  `IsBillSettled` tinyint(1) NOT NULL,
+  `IsBillSettled` tinyint(1) NOT NULL COMMENT '0 - Unpaid, 1 - Paid; ',
   `Status` varchar(1) NOT NULL COMMENT 'B - borrowed; R - Returned'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `borrow`
 --
 
 INSERT INTO `borrow` (`ID`, `BookFK`, `UserFK`, `FromDate`, `ToDate`, `IsBillSettled`, `Status`) VALUES
-(1, 1, 11, '2022-12-01', '2023-01-01', 1, 'B'),
-(6, 1, 16, '0000-00-00', '0000-00-00', 0, 'R'),
-(7, 1, 16, '2023-01-26', '2023-01-31', 0, 'R');
+(1, 1, 11, '2022-12-01', '2023-01-01', 0, 'R'),
+(6, 6, 16, '2023-01-15', '2023-01-17', 0, 'R'),
+(7, 4, 16, '2023-01-26', '2023-01-31', 0, 'B'),
+(9, 5, 17, '2023-01-09', '2023-01-24', 0, 'B'),
+(10, 4, 17, '2023-01-16', '2023-01-23', 0, 'B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt`
+--
+
+CREATE TABLE `receipt` (
+  `ID` int(11) NOT NULL,
+  `BorrowFK` int(11) NOT NULL,
+  `LateDay` int(11) NOT NULL,
+  `Penalty` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `receipt`
+--
+
+INSERT INTO `receipt` (`ID`, `BorrowFK`, `LateDay`, `Penalty`) VALUES
+(53, 1, 24, 12),
+(54, 6, 8, 4),
+(55, 7, 4, 0),
+(56, 9, 2, 1),
+(57, 10, 3, 1.5);
 
 -- --------------------------------------------------------
 
@@ -83,7 +108,6 @@ INSERT INTO `borrow` (`ID`, `BookFK`, `UserFK`, `FromDate`, `ToDate`, `IsBillSet
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `ID` int(11) NOT NULL,
   `Email` varchar(100) NOT NULL,
@@ -94,7 +118,7 @@ CREATE TABLE `user` (
   `FullName` varchar(100) NOT NULL,
   `UitmID` int(11) NOT NULL,
   `IsAdmin` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -131,6 +155,13 @@ ALTER TABLE `borrow`
   ADD KEY `UserFK` (`UserFK`);
 
 --
+-- Indexes for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `BorrowFK` (`BorrowFK`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -145,13 +176,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `borrow`
 --
 ALTER TABLE `borrow`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `receipt`
+--
+ALTER TABLE `receipt`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `user`
